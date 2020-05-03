e563db51231d726cc48585e4c705a12d0128b9bb
SpencerOfwiti <maxspencer56@gmail.com>
2020-05-03 19:41:39 +0300

    Add fixture to hold processed data.

--- src/tests/test_processed_dataset.py
+++ src/tests/test_processed_dataset.py
@@ -1,7 +1,21 @@
 import pandas as pd
+import pytest
 
-data = pd.read_csv('data/processed/Safaricom-Ltd(SCOM).csv')
+
+@pytest.fixture
+def processed_data():
+	"""
+	fixture holding processed dataset
+	:return:
+	"""
+	data = pd.read_csv('data/processed/Safaricom-Ltd(SCOM).csv')
+	return data
 
 
-def test_is_null_is_zero():
-	assert data.isnull().sum().sum() == 0
+def test_is_null_is_zero(processed_data):
+	"""
+	test if processed data has no null variables
+	:param processed_data:
+	:return:
+	"""
+	assert processed_data.isnull().sum().sum() == 0
