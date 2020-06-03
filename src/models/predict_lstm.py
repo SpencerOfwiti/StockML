2c67f2a3c77f1d5352e4f921c5ecee415c058d1a
SpencerOfwiti <maxspencer56@gmail.com>
2020-06-03 18:12:44 +0300

    Add LSTM model. - Train the model. - Predict using the model. - Visualize the models performance.

---
+++ src/models/predict_lstm.py
@@ -0,0 +1,51 @@
+import sys
+from joblib import load
+import pandas as pd
+import numpy as np
+
+sys.path.append('..')
+from scaling import scaler
+from rmse import rms
+from threshold import get_threshold
+
+# %% load dataset
+data = pd.read_csv('../../data/processed/Safaricom-Ltd(SCOM)-lstm.csv')
+
+# %% load the LSTM model
+model = load('../../models/lstm/Safaricom-Ltd(SCOM).pkl')
+
+# %% split data into test data
+data_list = data.values
+threshold = get_threshold(data_list)
+test = data_list[threshold:, :]
+
+# %% scale the data
+scaled_data = scaler.fit_transform(data_list)
+
+# %% create predictions for test data
+inputs = data[len(data) - len(test) - 60:].values
+inputs = inputs.reshape(-1, 1)
+inputs = scaler.transform(inputs)
+
+x_test = []
+for i in range(60, inputs.shape[0]):
+    x_test.append(inputs[i-60: i, 0])
+x_test = np.array(x_test)
+
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+predicted_price = model.predict(x_test)
+predicted_price = scaler.inverse_transform(predicted_price)
+
+# %% calculate RMSE
+rms = rms(test, predicted_price, 'LSTM')
+print('RMSE:', rms)
+
+# %% create dataset with price and prediction
+pred = pd.DataFrame(index=range(0, len(test)), columns=['Price', 'Prediction'])
+
+for i in range(0, len(test)):
+    pred['Price'][i] = test[i][0]
+    pred['Prediction'][i] = predicted_price[i][0]
+
+# %% save predicted data
+pred.to_csv('../../data/predicted/lstm/Safaricom-Ltd(SCOM).csv', index=False)
---
+++ src/models/train_lstm.py
@@ -0,0 +1,52 @@
+import sys
+from joblib import dump
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+
+sys.path.append('..')
+from threshold import get_threshold
+from scaling import scaler
+
+# %% load dataset
+data = pd.read_csv('../../data/interim/Safaricom-Ltd(SCOM).csv')
+data = data[['Date', 'Price']]
+data.index = data['Date']
+data.drop('Date', axis=1, inplace=True)
+
+# %% sort data in ascending order
+data = data.sort_index(ascending=True, axis=0)
+
+# %% save dataset for later use
+data.to_csv('../../data/processed/Safaricom-Ltd(SCOM)-lstm.csv', index=False)
+
+# %% split data into train data
+data_list = data.values
+threshold = get_threshold(data_list)
+train = data_list[:threshold, :]
+
+# %% scale the data
+scaled_data = scaler.fit_transform(data_list)
+
+# convert data to x_train and y_train
+x_train, y_train = [], []
+for i in range(60, len(train)):
+    x_train.append(scaled_data[i-60: i, 0])
+    y_train.append(scaled_data[i, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+# %% implement the LSTM network
+model = Sequential()
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(LSTM(units=50))
+model.add(Dense(1))
+
+# %% compile and fit the model
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(x_train, y_train, epochs=10, batch_size=1, verbose=2)
+
+# %% save model for later use
+dump(model, '../../models/lstm/Safaricom-Ltd(SCOM).pkl')
