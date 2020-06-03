8b997d1333694b84518907aa906e53836a0821a2
SpencerOfwiti <maxspencer56@gmail.com>
2020-06-03 17:59:46 +0300

    Add RMSE value dataset.

--- src/rmse.py
+++ src/rmse.py
@@ -1,6 +1,15 @@
+import csv
 import numpy as np
 
 
-def rms(actual, prediction):
-    rms = np.sqrt(np.mean(np.power((np.array(actual)-np.array(prediction)), 2)))
-    return rms
+def rms(actual, prediction, model):
+    rmse = np.sqrt(np.mean(np.power((np.array(actual)-np.array(prediction)), 2)))
+    data = [[model, rmse]]
+
+    file = open('../../data/report/rmse.csv', 'a+', newline='')
+
+    with file:
+        write = csv.writer(file)
+        write.writerows(data)
+
+    return rmse
