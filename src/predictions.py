4d8059a6132a8ad3faf6daf4e54cb3f029c2df39
Spencer Ofwiti <maxspencer56@gmail.com>
2021-02-03 21:48:56 +0300

    Add return of historical values.

--- src/predictions.py
+++ src/predictions.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
 
-def get_predictions(file):
-    predictions = pd.read_csv(file)
-    preds = [x for x in predictions['Prediction']]
-    return preds
+def get_predictions(historical_data, prediction_data):
+    history = pd.read_csv(historical_data)
+    history = history[['Date', 'Price']]
+    historical_values = dict(zip(history['Date'], history['Price']))
+    predictions = pd.read_csv(prediction_data)
+    prediction_values = [x for x in predictions['Prediction']]
+    return historical_values, prediction_values
