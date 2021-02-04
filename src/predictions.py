1d3b783699a1c9d67fa19c18cc6003887e1aae57
Spencer Ofwiti <maxspencer56@gmail.com>
2021-02-04 08:35:26 +0300

    Add change % to historical data.

--- src/predictions.py
+++ src/predictions.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 def get_predictions(historical_data, prediction_data):
     history = pd.read_csv(historical_data)
-    history = history[['Date', 'Price']]
-    historical_values = dict(zip(history['Date'], history['Price']))
+    history = history[['Date', 'Price', 'Change %']]
+    historical_values = dict(zip(history['Date'], list(zip(history['Price'], history['Change %']))))
     predictions = pd.read_csv(prediction_data)
     prediction_values = [x for x in predictions['Prediction']]
     return historical_values, prediction_values
