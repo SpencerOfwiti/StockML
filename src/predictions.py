fbf20781ae8ee559a22136014eb4411f3e0bf270
Spencer Ofwiti <maxspencer56@gmail.com>
2021-02-04 10:07:54 +0300

    Refactor historical data structure.

--- src/predictions.py
+++ src/predictions.py
@@ -4,7 +4,7 @@ import pandas as pd
 def get_predictions(historical_data, prediction_data):
     history = pd.read_csv(historical_data)
     history = history[['Date', 'Price', 'Change %']]
-    historical_values = dict(zip(history['Date'], list(zip(history['Price'], history['Change %']))))
+    historical_values = list(zip(history['Date'], history['Price'], history['Change %']))
     predictions = pd.read_csv(prediction_data)
     prediction_values = [x for x in predictions['Prediction']]
     return historical_values, prediction_values
