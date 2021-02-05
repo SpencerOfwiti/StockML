acaac30430c22607e1bbc2b66dfdc05a02d97c4a
Spencer Ofwiti <maxspencer56@gmail.com>
2021-02-05 09:16:14 +0300

    Refactor predictions format.

--- src/predictions.py
+++ src/predictions.py
@@ -5,8 +5,8 @@ def get_predictions(historical_data, prediction_data):
     history = pd.read_csv(historical_data)
     history = history[['Date', 'Price', 'Change %']]
     historical_values = list(zip(history['Date'], history['Price'], history['Change %']))
-    historical_values = [list(hist) for hist in historical_values]
+    historical_values = [{hist[0]: hist[1]} for hist in historical_values]
     predictions = pd.read_csv(prediction_data)
     prediction_values = list(zip(history['Date'], predictions['Prediction'][::-1]))
-    prediction_values = [list(pred) for pred in prediction_values]
+    prediction_values = [{pred[0]: pred[1]} for pred in prediction_values]
     return historical_values, prediction_values
